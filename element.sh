648ee75085d7260d130426d2838b9bc1e5215efc
kylestech95 <kylestech95@outlook.com>
2024-01-05 06:41:02 -0500

    feat: add argument

--- element.sh
+++ element.sh
@@ -1,2 +1,12 @@
 #! /bin/bash
 PSQL="psql -X --username=postgres --dbname=periodic_table --tuples-only -c"
+
+# if an argument is present
+if [[ $1 ]]
+    then
+    # keep going 
+
+else
+    # provide an arguemnt
+    echo "Please provide an element as an argument."
+fi
